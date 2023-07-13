acba7fc72a7e788934c3184338bab5d35ebd38f2
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-07-13 13:07:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function fibonacci(num) {
 // your code here
-	let numb=Number(num);
+	
 		
-	if(numb<=0 || numb==1){
+	if(num<=0 || num==1){
 
 		return 0;
 	}
 
 	let c=0;
+	let a=0, b=1;
 	
-	for(let i=2; i<=numb; i++){
+	for(let i=2; i<=num; i++){
 
-		let a=0, b=1;
 
-		 c=a+b;
+		c=a+b;
 
 		a=b;
 		b=c;
